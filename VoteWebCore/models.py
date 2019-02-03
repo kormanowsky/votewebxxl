417389c011161e6f94d1fae418996bf995c5911a
Михаил Кормановский <kormanowsky@gmail.com>
2019-02-03 23:53:34 +0300

    Remade database struct, created function is_logged_in()

--- VoteWebCore/models.py
+++ VoteWebCore/models.py
@@ -1,49 +1,64 @@
 from django.contrib.auth.models import User
 from django.db import models
+from json import dumps as json_encode, load as json_decode
+from VoteWebCore.functions import is_logged_in
 
 
-# Create your models here.
+class Voting(models.Model):
 
-class TB_Vote(models.Model):
-    # quiz_id = models.AutoField(primary_key=True)
-    vote_name = models.CharField(max_length=32, default='')
-    vote_type = models.IntegerField()
-    vote_owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, blank=False)
+    title = models.CharField(max_length=300)
 
-    # it was vote type
-    VT_DISCRET = 0
-    VT_MULTI = 1
+    # Returns list of all voting questions
+    def questions(self):
+        return Question.objects.filter(voting=self.id)
 
 
-class TB_VoteDiscret(models.Model):
-    vote_id = models.ForeignKey(to=TB_Vote, on_delete=models.CASCADE)
-    vote_no = models.IntegerField()
-    vote_text_question = models.CharField(max_length=256)
-    vote_text_yes = models.CharField(max_length=16)
-    vote_text_no = models.CharField(max_length=16)
+class Question(models.Model):
+
+    # Question types
+    # Question with two buttons (usually 'yes' and 'no')
+    QUESTION_BUTTONS = 0
+    # Question with radio inputs
+    QUESTION_SINGLE_ANSWER = 1
+    # Question with checkboxes
+    QUESTION_MULTIPLE_ANSWERS = 2
+
+    voting = models.ForeignKey(to=Voting, on_delete=models.CASCADE)
+    type = models.IntegerField()
+    text = models.CharField(max_length=300)
+    answers = models.CharField(max_length=10000)
+
+    # In __init__() we decode answers
+    def __init__(self, *args, **kwargs):
+        super(models.Model, self).__init__(self, *args, **kwargs)
+        self.answers = json_decode(self.answers)
+
+    # In save() we encode answers
+    def save(self, *args, **kwargs):
+        self.answers = json_encode(self.answers)
+        super(models.Model, self).save(*args, **kwargs)
+
+    # Returns question statistics for diagram
+    def stats(self):
+        stats = {
+            "all": Vote.objects.filter(question=self.id),
+        }
+        for answer in self.answers:
+            stats[answer] = Vote.objects.filter(question=self.id, answer=answer)
+        return stats
+
+    # Checks if current user has voted
+    def current_user_voted(self, request):
+        if not is_logged_in(request):
+            return False
+        return len(Vote.objects.filter(question=self.id, creator=request.user.id)) == 1
 
 
-class TB_VoteLog(models.Model):
-    answ_user_id = models.IntegerField()
-    vote_id = models.IntegerField()
-    vote_no = models.IntegerField()
-    answer_no = models.IntegerField(default=-1)
+class Vote(models.Model):
 
-
-class TB_VoteMulti(models.Model):
-    vote_id = models.ForeignKey(to=TB_Vote, on_delete=models.CASCADE)
-    # vote_no = models.IntegerField()
-    quests_no = models.IntegerField()
-
-
-class TB_VoteMulti_Quest(models.Model):
-    quests_no = models.OneToOneField(to=TB_VoteMulti, on_delete=models.CASCADE)
-    quest_numerate = models.IntegerField()
-    quest_text = models.CharField(max_length=16)
-
-
-class TB_VoteMulti_Answer(models.Model):
-    quests_no = models.IntegerField()
-    quest_numerate = models.IntegerField()
-    answer_numerate = models.IntegerField()
-    answer_text = models.CharField(max_length=16)
+    creator = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, blank=False)
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
