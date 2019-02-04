7aa22debd181f105e3b46467972ad29ed019211b
Михаил Кормановский <kormanowsky@gmail.com>
2019-02-04 13:26:47 +0300

    Fixed input name in votings.js

--- VoteWebCore/static/js/votings.js
+++ VoteWebCore/static/js/votings.js
@@ -3,7 +3,7 @@ Votings.setCurrentButtonAnswer = function(element){
     var $element = $(element),
         answer = $element.text().trim(),
         question_id = $element.attr('data-question-id'),
-        $input = $("input[name='answers[" + question_id + "]'");
+        $input = $("input[name='answer_" + question_id + "']");
     $input.attr('value', answer);
     $element.siblings("button.btn-primary").toggleClass("btn-outline-primary btn-primary");
     $element.toggleClass("btn-outline-primary btn-primary");
