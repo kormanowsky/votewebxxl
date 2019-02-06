9af032d66b58e34cf20b035cf62ca70b36050cbd
Михаил Кормановский <kormanowsky@gmail.com>
2019-02-06 23:42:24 +0300

    Added report status on report creation

--- VoteSimple/urls.py
+++ VoteSimple/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from VoteWebCore import views, api_views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('votings', views.votings),
 
     # Auth module
