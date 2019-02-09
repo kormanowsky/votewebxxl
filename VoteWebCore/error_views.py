0a7dbc83dc7c998e784917345c06f8041f7462e1
Михаил Кормановский <kormanowsky@gmail.com>
2019-02-09 14:28:16 +0300

    Replaced many error templates with one template

--- VoteWebCore/error_views.py
+++ VoteWebCore/error_views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 
 def error_bad_request(request):
-    return render(request, 'error/400.html', {}, status=400)
+    return render(request, 'error.html', {
+        "code": 400,
+        "text": "Bad Request"
+    }, status=400)
 
 def error_forbidden(request):
-    return render(request, 'error/403.html', {}, status=403)
+    return render(request, 'error.html', {
+        "code": 403,
+        "text": "Forbidden"
+    }, status=403)
 
 def error_not_found(request, exception=None):
-    return render(request, 'error/404.html', {}, status=404)
+    return render(request, 'error.html', {
+        "code": 404,
+        "text": "Not Found"
+    }, status=404)
 
 def error_method_not_allowed(request):
-    return render(request, 'error/405.html', {}, status=405)
+    return render(request, 'error.html', {
+        "code": 405,
+        "text": "Method Not Allowed"
+    }, status=405)
 
 def error_internal(request):
-    return render(request, 'error/500.html', {}, status=500)
+    return render(request, 'error.html', {
+        "code": 500,
+        "text": "Internal Server Error"
+    }, status=500)
 
 def error_csrf(request):
-    return render(request, 'error/403.html', {}, status=403)
+    return render(request, 'error.html', {
+        "code": 403,
+        "text": "Forbidden"
+    }, status=403)
